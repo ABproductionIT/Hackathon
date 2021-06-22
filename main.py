2e735ee57e262fe769fb2b74fa16d80094aeb8a7
ABproductionIT <79978141+ABproductionIT@users.noreply.github.com>
2021-06-22 22:43:05 +0400

    load

---
+++ main.py
@@ -0,0 +1,21 @@
+def bear(berries, n):
+    while berries[0:n:2] > berries[1:n:2]:
+        return str(berries[0:n:2])
+    else:
+        return str(berries[1:n:2])
+
+
+file = open("input.txt", "r")
+fileout = open('output.txt', 'w')
+forestLen = len(file.readlines())
+with open("input.txt") as f:
+    forest = f.readlines()
+    for i in range(forestLen):
+        level = forest[i].split()
+        level = list(map(int, level))
+        levelLen = len(level)
+        a = bear(level, levelLen)
+        fileout.writelines(a + "\n")
+        print(a)
+fileout.close()
+file.close()
